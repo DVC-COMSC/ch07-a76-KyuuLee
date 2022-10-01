e25cfe69e73126c9ab6b2efd359351e2165065f0
Kyuu <69904775+KyuuLee@users.noreply.github.com>
2022-10-01 14:19:38 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-
 num1 = list(map(int, input().split()))
 num2 = list(map(int, input().split()))
+N = len(num1) 
+M = len(num2) 
 
-# ******************************
-# Make your Code
-# ******************************
+if N<M:
+	num3 = num1 + num2
+else:
+	num3 = num2 + num1
+print (num3)
 
-# print (num3) 
